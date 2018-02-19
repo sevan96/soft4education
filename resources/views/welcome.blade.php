31b622b52bde1841e7eea4142b4d8deecbf30520
sevanAkoumia <sevanakoumia@gmail.com>
2018-02-19 11:22:23 +0000

    Modification de la page d'accueil

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -79,7 +79,7 @@
 
             <div class="content">
                 <div class="title m-b-md">
-                    Soft4Education
+                    Soft4Education - 2018
                 </div>
 
                 <div class="links">
